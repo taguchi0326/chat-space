722e43a522e4877427015dbfd6488a830f56caee
taguchi0326 <tagsow@gmail.com>
2020-05-14 12:56:22 +0900

    chat-space

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
     if ( message.image ) {
       var html =
-       `<div class="message">
+       `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
       return html;
     } else {
       var html =
-       `<div class="message">
+       `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
             <div class="upper-message__user-name">
               ${message.user_name}
@@ -39,7 +39,7 @@ $(function(){
       return html;
     };
   }
-$('#new_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
@@ -57,9 +57,38 @@ $('#new_message').on('submit', function(e){
        $('.chat-main__massage-list').animate({ scrollTop: $('.chat-main__massage-list')[0].scrollHeight});
        $('#new_message')[0].reset();
        $('.chat-main__form__spase__submit').prop( 'disabled', false );
-      })
-      .fail(function() {
-        alert("メッセージ送信に失敗しました");
-      });
-    });
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    })
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+         var insertHTML = '';
+         $.each(messages, function(i, message) {
+           insertHTML += buildHTML(message)
+         });
+        $('.chat-main__massage-list').append(insertHTML);
+        $('.chat-main__massage-list').animate({ scrollTop: $('.chat-main__massage-list')[0].scrollHeight});
+      }
+         console.log('success');
+        })
+        .fail(function() {
+        alert('error');
+        });
+  };
+  $(function() {
+  });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+});
