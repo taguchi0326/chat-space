a8bf3d96def0a061178cac2d41783f9482b2edd3
taguchi0326 <tagsow@gmail.com>
2020-05-15 23:00:12 +0900

    dd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+
+  server '54.150.251.124', user: 'ec2-user', roles: %w{app db web}
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
